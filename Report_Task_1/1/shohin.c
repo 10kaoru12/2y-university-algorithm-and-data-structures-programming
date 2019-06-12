d0a9f546ae7dd01102f459436a7e52ce1defe5fd
10kaoru12 <10kaoru12@gmail.com>
2019-06-12 20:05:24 +0900

    問題1修正

--- Report_Task_1/1/shohin.c
+++ Report_Task_1/1/shohin.c
@@ -12,13 +12,15 @@ SHOHIN shohin[] = {{"Apple", 150}, {"Orange", 100}, {"Banana", 200}, {"Book1", 5
 
 void printshohin(SHOHIN s)
 {
+  char *uti = "内税";
+  char *soto = "外税";
   if (s.sotozei == 0)
   {
-    printf("%s\t単価%d円(内税)", s.name, s.tanka);
+    printf("%s\t単価%d円(%s)", s.name, s.tanka,uti);
   }
   else
   {
-    printf("%s\t単価%d円(外税)", s.name, s.tanka);
+    printf("%s\t単価%d円(外税)", s.name, s.tanka,soto);
   }
 }
 int main(void)
